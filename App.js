26d7b5d99633233fb2599527e827d9b910547c2e
Roni <roni.c.castro@gmail.com>
2018-12-30 14:56:17 -0200

    Add status bar

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { View } from 'react-native';
 import Tabs from './components/Tabs'
+import { black } from './utils/colors'
+import CustomStatusBar from './components/CustomStatusBar';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={{flex: 1}}>
+        <CustomStatusBar backgroundColor={black} barStyle='light-content' />
         <Tabs />
       </View>
     );
