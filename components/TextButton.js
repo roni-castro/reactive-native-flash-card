05884813b0c44693a8842d8bd5d6b9e7406b9679
Roni <roni.c.castro@gmail.com>
2018-12-31 04:41:16 -0200

    Create components for new deck screen

---
+++ components/TextButton.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { TouchableOpacity, Text, StyleSheet, Platform } from 'react-native'
+import { black, white } from '../utils/colors';
+
+export default function TextButton({text, onPress, style = {}}) {
+    return (
+        <TouchableOpacity style={[styles.button, style]} onPress={onPress}>
+             <Text style={styles.text}>{text}</Text>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    text: {
+      textAlign: 'center',
+      color: white,
+      padding: 8
+    },
+    button: {
+        backgroundColor: black,
+        borderWidth: 1,
+        borderRadius: Platform.OS === 'ios' ? 16 : 4,
+    }
+})
